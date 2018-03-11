e854431b996bf0954671c653050869e4ebe6dc9b
Hizal Celik <hizalcelik@gmail.com>
2018-03-10 21:27:03 -0500

    added more functionality to processor

--- prepare_dataset/prepare_dataset.py
+++ prepare_dataset/prepare_dataset.py
@@ -1,8 +1,11 @@
 from PIL import Image
 import os
+import sys
 
-img_folder = "/Users/lingdonghuang/Downloads/leftImg8bit_trainvaltest/leftImg8bit/train"
-ann_folder = "/Users/lingdonghuang/Downloads/gtFine_trainvaltest/gtFine/train"
+img_folder = "../../leftImg8bit_trainvaltest/leftImg8bit/train"
+ann_folder = "../../gtFine_trainvaltest/gtFine/train"
+
+print "-- Dataset Preparation --"
 
 def smart_resize(im,w,h):
     iw,ih = im.size
@@ -29,22 +32,43 @@ def smart_resize(im,w,h):
 if __name__ == "__main__":
     cities = os.listdir(img_folder)
 
+    img_total = 0;
+    for i in range(len(cities)):
+        img_total += len(os.listdir(img_folder+"/"+cities[i]))
+    print str(img_total)+" images across "+str(len(cities))+" folders will be processed.\n"
+
+    maxphotos = raw_input("Max photos (null=all): ")
+    maxphotos = int(maxphotos) if len(maxphotos) else img_total
+
+    width = raw_input("Width (null=512): ")
+    width = int(width) if len(width) else 512
+    height = raw_input("Height (null=512): ")
+    height = int(height) if len(height) else 512
+
+    counter = 0
     for i in range(len(cities)):
         fnames0 = os.listdir(img_folder+"/"+cities[i])
         fnames1 = map((lambda x: "_".join(x.split("_")[:-1])+"_gtFine_color.png"), fnames0)
 
+        if(counter > maxphotos): break
+        print "\nProcessing /" + cities[i]
+
         for j in range(0,len(fnames0)):
+            counter += 1
+            if(counter > maxphotos): break
             f0 = img_folder+"/"+cities[i]+"/"+fnames0[j]
             f1 = ann_folder+"/"+cities[i]+"/"+fnames1[j]
             im0 = Image.open(f0)
             im1 = Image.open(f1)
-            im0 = smart_resize(im0,512,512)
-            im1 = smart_resize(im1,512,512)
+            im0 = smart_resize(im0,width,height)
+            im1 = smart_resize(im1,width,height)
             im = Image.new('RGB',(im0.size[0]+im1.size[0],im0.size[1]))
             im.paste(im0,(0,0))
             im.paste(im1,(im0.size[0],0))
             #im.show()
             fname = "_".join(fnames0[j].split("_")[:-1])
             im.save("out/"+fname+".png","PNG")
+            sys.stdout.write('.')
+    print "\n"
 
         
